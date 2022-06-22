d3c77bae8acc05ac2fca986740172647e566b4f1
muhammadhendro <muhammadhendro88@gmail.com>
2022-06-22 11:18:23 +0700

    initial commit

---
+++ app/api/v1/categories/router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express();
+
+router.get('/categories', (req, res) => {
+    res.status(200).json({
+        message: "Halaman categories"
+    });
+});
+
+module.exports = router;
+
